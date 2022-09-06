f0d3b677553832c7d9f8d99ca4cd5ff8f95b240d
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-06 22:30:41 +0530

    ui completed

--- react-assignnment/src/components/Post.jsx
+++ react-assignnment/src/components/Post.jsx
@@ -7,15 +7,23 @@ export const Post=()=>{
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json()).then(data=>setData(data))
     },[])
-   
+
     console.log(data)
+  
     return(
         <div>
+             <div className="inp-div">
+                    <input type="text" placeholder="Search" />
+                    <button>submit</button>
+                </div>
             <div className="main-div">
+               
                 {data.map((e)=>{
                     return(
                         <div className="sub-div">
-                            <p>{e.title}</p>
+                            <p> <span>Title :</span> {e.title}</p>
+                            <p><span>Body :</span> {e.body}</p>
+                            <button>User Details</button>
                         </div>
                     )
                 })}
