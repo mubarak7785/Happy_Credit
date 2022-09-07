749277299466df58f66501559cf7af5a42855ee5
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-07 12:33:57 +0530

    search functionality added

--- react-assignnment/src/components/Post.jsx
+++ react-assignnment/src/components/Post.jsx
@@ -3,27 +3,49 @@ import "./Post.css"
 export const Post=()=>{
 
     const[data,setData]=useState([])
+    const [fdata,setFdata]=useState([])
+    const[search,setSearch]=useState("")
 
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json()).then(data=>setData(data))
     },[])
 
     console.log(data)
+
+    const searchChange=(e)=>{
+        const {value}= e.target;
+        setSearch(value)
+        if(search.length<2){
+            setFdata(data)
+        }
+        else{
+            const fdatab=data.filter((elem)=>{
+               return elem.title.toLowerCase().includes(search.toLowerCase())
+            })
+            setFdata(fdatab)
+        }
+    }
+    
+
+    const handleChange=(elem)=>{
+        console.log(elem)
+    }
   
     return(
         <div>
+            <h1>Happy Credit</h1>
              <div className="inp-div">
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search" onChange={searchChange} />
                     <button>submit</button>
                 </div>
             <div className="main-div">
                
-                {data.map((e)=>{
+                {fdata.map((elem)=>{
                     return(
                         <div className="sub-div">
-                            <p> <span>Title :</span> {e.title}</p>
-                            <p><span>Body :</span> {e.body}</p>
-                            <button>User Details</button>
+                            <p> <span>Title :</span> {elem.title}</p>
+                            <p><span>Body :</span> {elem.body}</p>
+                            <button onClick={handleChange}>User Details</button>
                         </div>
                     )
                 })}
